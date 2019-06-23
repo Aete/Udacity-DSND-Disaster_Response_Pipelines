0d0dc110251d328d8fd3229afe885d7eb5a52082
Aete <hsg8234@naver.com>
2019-06-23 18:30:26 +0900

    Write Docstring for ML_pipeline jupyter notebook
    1. Write Docstring for ML_pipeline jupyter notebook
    
    2. Revise ETL pipeline jupyter notebook
    - assert when the number of duplicate value is 0
    - put the if_exists='replace' to df.to_sql()

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -41,6 +41,7 @@ def tokenize(text):
     text = re.sub(r"[^a-zA-Z]", " ", text.lower())
     
     # replace url to urlplaceholder
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     detected_urls = re.findall(url_regex, text)
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
